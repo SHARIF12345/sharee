958800b211d74721ad745ba252bf60252d6ce4d2
QA12 <QA12@QAPLANET-PC>
2019-08-21 15:56:24 +0530

    Cucumber with maven

---
+++ cucumber.maven/src/test/java/Maven/cucumber/maven/Testrun.java
@@ -0,0 +1,13 @@
+package Maven.cucumber.maven;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="feature")
+public class Testrun {
+ 
+}
+//glue={"scripting"}
